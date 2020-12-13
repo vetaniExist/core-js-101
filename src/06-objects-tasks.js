da57536f130a01dca9aa5bd31faef569a53b6695
Huriosity <vetani3123@mail.ru>
2020-12-14 00:26:27 +0300

    feat: complete some object tasks

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -20,8 +20,20 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, heigh) {
+  return {
+    getArea() {
+      return width * heigh;
+    },
+
+    get width() {
+      return width;
+    },
+
+    get height() {
+      return heigh;
+    },
+  };
 }
 
 
@@ -35,8 +47,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 
@@ -51,8 +63,9 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = Object.create(proto);
+  return Object.assign(obj, JSON.parse(json));
 }
 
 
@@ -110,33 +123,79 @@ function fromJSON(/* proto, json */) {
  *  For more examples see unit tests.
  */
 
+class SelectorBuilder {
+  constructor(element) {
+    this.selector = element;
+  }
+
+  createID(id) {
+    this.selector = this.selector.concat(`#${id}`);
+  }
+
+  createClass(newClass) {
+    this.selector = this.selector.concat(`.${newClass}`);
+  }
+
+  createAttr(attr) {
+    this.selector = this.selector.concat(`[${attr}]`);
+  }
+
+  createPseudoClass(pClass) {
+    this.selector = this.selector.concat(`:${pClass}`);
+  }
+
+  createPseudoElement(pElement) {
+    this.selector = this.selector.concat(`::${pElement}`);
+  }
+
+  stringify() {
+    const result = this.selector;
+    this.selector = '';
+    return result;
+  }
+}
+
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  elems: [],
+
+  element(value) {
+    this.elem = new SelectorBuilder(value);
+    this.elems.push(this.elem);
+    return this;
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    // this.ID = value;
+    this.elem.createID(value);
+    return this;
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    this.elem.createClass(value);
+    return this;
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    this.elem.createAttr(value);
+    return this;
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    this.elem.createPseudoClass(value);
+    return this;
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    this.elem.createPseudoElement(value);
+    return this;
   },
 
   combine(/* selector1, combinator, selector2 */) {
     throw new Error('Not implemented');
+  },
+
+  stringify() {
+    return this.elem.stringify();
   },
 };
 
